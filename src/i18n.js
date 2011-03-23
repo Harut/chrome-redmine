1d04fc11b1d0ae3ce00c1438f7400d15af355175
Harut <none@none>
2011-03-23 21:48:32 +0300

    added i18n

---
+++ src/i18n.js
@@ -0,0 +1,57 @@
+window.i18n = {}
+
+window.i18n.en = {
+    'Add tab': '',
+    'Delete tab': '',
+    'Count updated issues': '',
+    'Show "assignee" column': '',
+    'Show "project" column': '',
+    'Show "status" column': '',
+    'will be used for Redmine API requests.': '',
+    'Filter': '',
+    'Title': '',
+    'Priorities': '',
+    'Tabs and filters': '',
+    'Update interval (in minutes)': '',
+    'Redmine URL': '',
+    'Common options': '',
+    'Save': '',
+    'Locale': ''
+}
+
+window.i18n.ru = {
+    'Add tab': 'Добавить вкладку',
+    'Delete tab': 'Удалить вкладку',
+    'Count updated issues': 'Учитывать при подсчете обновлений',
+    'Show "assignee" column': 'Показывать колонку исполнителя',
+    'Show "project" column': 'Показывать колонку проекта',
+    'Show "status" column': 'Показывать колонку статуса',
+    'will be used for Redmine API requests.': 'будет использоваться для запроса к API Redmine.',
+    'Filter': 'Фильтр',
+    'Title': 'Название',
+    'Priorities': 'Приоритеты',
+    'Tabs and filters': 'Вкладки и фильтры',
+    'Update interval (in minutes)': 'Интервал обновлений (в мин)',
+    'Redmine URL': 'URL на Redmine',
+    'Common options': 'Главные настройки',
+    'Save': 'Сохранить',
+    'Locale': 'Язык'
+}
+
+
+window.currentLocale = localStorage.currentLocale || 'en';
+
+function _(s){
+    if (i18n[currentLocale] && i18n[currentLocale][s]){
+        return i18n[currentLocale][s];
+    }
+    return s;
+}
+
+window.addEventListener('DOMContentLoaded', function(){
+    var i18n_elems = document.getElementsByClassName('i18n');
+    console.log(i18n_elems);
+    for (var i=i18n_elems.length; i--;){
+        i18n_elems[i].innerText = _(i18n_elems[i].innerText);
+    }
+});
